d8927d32baa46be74f295245d80659af8e1020f3
omar nazih <omarnazihcs@gmail.com>
2023-09-04 20:57:29 +0300

    remove console logs

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Footer() {
-  console.log(new Date().getFullYear());
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
